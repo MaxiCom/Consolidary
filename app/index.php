094f507b849f74120ceee84b71c2f5d4d1354d2e
maxime morlet <maximemorlet@hotmail.fr>
2016-11-30 03:04:13 +0100

    First commit

---
+++ app/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<?php include("views/head.html"); ?>
+	</head>
+	<body>
+		<div id="loader_wrapper">
+			<h1 class="hidden"><span class="hidden">Consolidary</span></h1>
+		</div>
+		<div id="container">
+			<nav>
+				<div id="nav_wrapper">
+					<ul>
+						<li>Web</li>
+						<li>Compilation</li>
+						<li>Sécurité</li>
+						<li>Réseau</li>
+						<li>Python</li>
+						<li>C / C++</li>
+						<li>Assembleur</li>
+					</ul>
+				</div>
+			</nav>			
+		</div>
+
+		<!-- Font and Libs -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+
+		<!-- Libs and Scipts -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script src="//code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+		<script type="text/javascript" src="scripts/loader.js"></script>
+	</body>
+</html>
